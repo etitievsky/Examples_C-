80a1ce3979ab671627832fe0782b70107d278cd5
etitievsky <etitievsky@mail.ru>
2022-12-01 20:12:43 +0300

    Example 005 Condition If/Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "таня")
+{
+Console.WriteLine("Ура! Это же Таня!");
+}
+else
+{
+Console.Write("Привет,"); Console.WriteLine(username);
+}
+
+
+
